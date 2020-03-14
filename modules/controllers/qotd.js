20b234ab8d6a8eea1119dafc1c6af036259ee57d
0x736a64 <1a2e1ef0@opayq.com>
2020-03-14 13:15:23 -0400

    add args for server

--- modules/controllers/qotd.js
+++ modules/controllers/qotd.js
@@ -6,7 +6,13 @@ const auth = require("../../private/auth.json");
 const config = require("../../config.json");
 
 const scrape = (async (guilds) => {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: [
+            'no-sandbox',
+            'disable-setuid-sandbox',
+        ]
+    });
     const page = await browser.newPage();
     await page.goto("https://www.reddit.com/r/InsightfulQuestions/");
     
