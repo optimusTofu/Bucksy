40d3ca28101cb887fef3e048730ad998a759c350
0x736a64 <1a2e1ef0@opayq.com>
2020-03-19 00:18:42 -0400

    wip: get help module to consume commands

--- modules/controllers/help.js
+++ modules/controllers/help.js
@@ -1,19 +1,21 @@
 const config = require("../../config.json");
+const commandController = require("./commands.js");
 const Discord = require("discord.js");
 
 const showCommands = function (msg) {
+    console.log(this, commandController);
     let helpMsg = new Discord.RichEmbed()
     .setColor(0x000099)
     .setTitle(`${config.botName} Commands`)
     .setDescription(`${config.botName} will listen to these commands if typed with a !, ., or & typed before them, like !help.`);
 
-    Object.keys(commands).forEach(cmd => {
-        let commandDescription = commands[cmd].description;
-        let aliases = cmd.split(",");
-        helpMsg.addField(aliases, commandDescription, true);
-    });
+    // Object.keys(commands).forEach(cmd => {
+    //     let commandDescription = commands[cmd].description;
+    //     let aliases = cmd.split(",");
+    //     helpMsg.addField(aliases, commandDescription, true);
+    // });
     
-    msg.channel.send(helpMsg);
+    // msg.channel.send(helpMsg);
 };
 
 module.exports = {
