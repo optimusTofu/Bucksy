665bf5063bb06a8cda4f9ee3a32278053f9950b1
0x736a64 <1a2e1ef0@opayq.com>
2020-03-22 13:34:12 -0400

    update emojies from config

--- modules/controllers/slots.js
+++ modules/controllers/slots.js
@@ -5,21 +5,21 @@ const databaseController = require("./database");
 const config = require("../../config.json");
 
 const emojies = [
-    "<:nanab_berry:690047532687949832>",
-    "<:nanab_berry:690047532687949832>",
-    "<:nanab_berry:690047532687949832>",
-    "<:nanab_berry:690047532687949832>",
-    "<:nanab_berry:690047532687949832>",
-    "<:pinap_berry:690047623729774677>",
-    "<:pinap_berry:690047623729774677>",
-    "<:pinap_berry:690047623729774677>",
-    "<:pinap_berry:690047623729774677>",
-    "<:razz_berry:690047698975326234>",
-    "<:razz_berry:690047698975326234>",
-    "<:razz_berry:690047698975326234>",
-    "<:silver_pinap_berry:690047733813608448>",
-    "<:silver_pinap_berry:690047733813608448>",
-    "<:golden_razz_berry:690047657149857794>"
+    config.emojies.nanab,
+    config.emojies.nanab,
+    config.emojies.nanab,
+    config.emojies.nanab,
+    config.emojies.nanab,
+    config.emojies.pinap,
+    config.emojies.pinap,
+    config.emojies.pinap,
+    config.emojies.pinap,
+    config.emojies.razz,
+    config.emojies.razz,
+    config.emojies.razz,
+    config.emojies.silver_pinap,
+    config.emojies.silver_pinap,
+    config.emojies.golden_razz
 ];
 
 const generateRandomEmoji = function(max) {
@@ -29,7 +29,7 @@ const generateRandomEmoji = function(max) {
 const spin = function(msg) {
     if (msg.channel.id !== config.channels.slots) return;
 
-    databaseController.updateBalance(msg.member.id, -1);
+    databaseController.updateBalance(msg, msg.member.id, -1);
 
     let first = generateRandomEmoji(emojies.length);
     let second = generateRandomEmoji(emojies.length);
@@ -49,38 +49,38 @@ const spin = function(msg) {
 
         switch (first) {
             case emojies[0]:
-                databaseController.updateBalance(msg.member.id, 50);
+                databaseController.updateBalance(msg, msg.member.id, 50);
                 winMsg = new Discord.RichEmbed()
                     .setFooter("You Won! 50 PokeCoins have been added to your account.", icon)
-                    .addField("Slot Machine Results <:pokecoin:690199453751443476>", result, true)
+                    .addField(`Slot Machine Results ${config.emojies.pokecoin}`, result, true)
                     .setColor(0x006600);
                 break;
             case emojies[5]:
-                databaseController.updateBalance(msg.member.id, 100);
+                databaseController.updateBalance(msg, msg.member.id, 100);
                 winMsg = new Discord.RichEmbed()
                     .setFooter("You Won! 100 PokeCoins have been added to your account.", icon)
-                    .addField("Slot Machine Results <:pokecoin:690199453751443476>", result, true)
+                    .addField(`Slot Machine Results ${config.emojies.pokecoin}`, result, true)
                     .setColor(0x006600);
                 break;
             case emojies[9]:
-                databaseController.updateBalance(msg.member.id, 500);
+                databaseController.updateBalance(msg, msg.member.id, 500);
                 winMsg = new Discord.RichEmbed()
                     .setFooter("You Won! 500 PokeCoins have been added to your account.", icon)
-                    .addField("Slot Machine Results <:pokecoin:690199453751443476>", result, true)
+                    .addField(`Slot Machine Results ${config.emojies.pokecoin}`, result, true)
                     .setColor(0x006600);
                 break;
             case emojies[12]:
-                databaseController.updateBalance(msg.member.id, 1000);
+                databaseController.updateBalance(msg, msg.member.id, 1000);
                 winMsg = new Discord.RichEmbed()
                     .setFooter("You Won! 1000 PokeCoins have been added to your account.", icon)
-                    .addField("Slot Machine Results <:pokecoin:690199453751443476>", result, true)
+                    .addField(`Slot Machine Results ${config.emojies.pokecoin}`, result, true)
                     .setColor(0x006600);
                 break;
             case emojies[14]:
-                databaseController.updateBalance(msg.member.id, 5000);
+                databaseController.updateBalance(msg, msg.member.id, 5000);
                 winMsg = new Discord.RichEmbed()
                     .setFooter("You Won! 5000 PokeCoins have been added to your account.", icon)
-                    .addField("Slot Machine Results <:pokecoin:690199453751443476>", result, true)
+                    .addField(`Slot Machine Results ${config.emojies.pokecoin}`, result, true)
                     .setColor(0x006600);
                 break;
         };
