69c368d9c3850957a28d589153a5f3a0169c1abf
0x736a64 <1a2e1ef0@opayq.com>
2021-03-12 22:15:46 -0500

    update for location mentions

--- controllers/notify.js
+++ controllers/notify.js
@@ -3,10 +3,11 @@ module.exports = {
         let text = msg.embeds[0].description;
         let pokemonName = text.split(" ")[0].toLowerCase();
         let pokemonRole = msg.guild.roles.cache.find(role => pokemonName.startsWith(role.name.toLowerCase()));
-        let locationName = msg.embeds[0].title;
+        let locationName = msg.embeds[0].title.toLowerCase;
+        let locationRole = msg.guild.roles.cache.find(role => locationName.startsWith(role.name.toLowerCase()));
 
         if (pokemonRole) {
-            msg.channel.send(`A wild ${pokemonRole} appeared in ${locationName}`);
+            msg.channel.send(`A wild ${pokemonRole} appeared in ${locationRole}`);
         }
     }
 }
