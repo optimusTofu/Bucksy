cb6d19da265c9492b3c7bf8b6fd485b6aed612ab
0x736a64 <1a2e1ef0@opayq.com>
2021-03-12 20:21:36 -0500

    update for hoppip v.3.0

--- controllers/notify.js
+++ controllers/notify.js
@@ -1,9 +1,9 @@
 module.exports = {
     pokemon: (msg) => {
-        let text = msg.embeds[0].title;
-        let pokemonName = text.split("**")[1].split(" ")[0].toLowerCase();
-        let pokemonRole = msg.guild.roles.cache.find(role => role.name.toLowerCase() === pokemonName);
-        let locationName = text.split("|")[1];
+        let text = msg.embeds[0].description;
+        let pokemonName = text.split(" ")[0].toLowerCase();
+        let pokemonRole = msg.guild.roles.cache.find(role => pokemonName.startsWith(role.name.toLowerCase()));
+        let locationName = msg.embeds[0].title;
 
         if (pokemonRole) {
             msg.channel.send(`${pokemonRole} - ${locationName}`);
