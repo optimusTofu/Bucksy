210d3532c46e3de5c9e7a00cdeac8709fc66b2a7
0x736a64 <1a2e1ef0@opayq.com>
2021-03-14 14:14:45 -0400

    update debug on notification

--- controllers/notify.js
+++ controllers/notify.js
@@ -4,18 +4,17 @@ module.exports = {
         let text = msg.embeds[0].description;
         let pokemonName = text.split(" ")[0].toLowerCase();
         let pokemonRole = msg.guild.roles.cache.find(role => pokemonName.startsWith(role.name.toLowerCase()));
-        let locationName = msg.embeds[0].title.toLowerCase;
-        //let locationRole = msg.guild.roles.cache.find(role => locationName.startsWith(role.name.toLowerCase()));
+        let locationName = msg.embeds[0].title.toLowerCase();
+        let locationRole = msg.guild.roles.cache.find(role => locationName.startsWith(role.name.toLowerCase()));
 
-        console.log(text);
         console.log(pokemonName);
         console.log(pokemonRole);
 
         console.log(locationName);
-        //console.log(locationRole);
+        console.log(locationRole);
 
         if (pokemonRole) {
-            //msg.channel.send(`A wild ${pokemonRole} appeared in ${locationRole}`);
+            msg.channel.send(`A wild ${pokemonRole} appeared in ${locationRole}`);
         }
     }
 }
