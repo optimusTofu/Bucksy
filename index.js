5b9021557705baabe2183298a6878455446ca835
0x736a64 <1a2e1ef0@opayq.com>
2021-08-12 10:34:27 -0400

    remove winston logger

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { Client, Collection } = require("discord.js");
 const fs = require("fs");
 const commands = fs.readdirSync("./commands");
 const events = fs.readdirSync("./events").filter((file) => file.endsWith(".js"));
-const { transports, createLogger, format } = require("winston");
 const dotenv = require("dotenv");
 const config = require("./config.json");
 const bot = new Client();
@@ -12,28 +11,6 @@ dotenv.config();
 bot.config = config;
 bot.commands = new Collection();
 bot.cooldowns = new Collection();
-bot.log = new createLogger({
-  level: "debug",
-  format: format.combine(format.json(), format.timestamp()),
-  defaultMeta: { service: "bot-service" },
-  exitOnError: false,
-  transports: [
-    new transports.File({ filename: "logs/error.log", level: "error" }),
-    new transports.File({ filename: "logs/info.log", level: "info" }),
-    new transports.File({ filename: "logs/debug.log", level: "debug" }),
-  ],
-  exceptionHandlers: [
-    new transports.File({ filename: 'logs/exceptions.log' })
-  ]
-});
-
-if (process.env.NODE_ENV !== "production") {
-  bot.log.add(
-    new transports.Console({
-      format: format.combine(format.json(), format.timestamp()),
-    })
-  );
-}
 
 for (const folder of commands) {
   const commandFiles = fs
